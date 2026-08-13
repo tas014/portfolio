525dee235c93f3f0dfe7248ea10da04282d7d817
Franco Picco <55250094+tas014@users.noreply.github.com>
2026-08-13 16:52:48 -0300

    Change default language from 'en' to 'es'

--- src/stores/lang.ts
+++ src/stores/lang.ts
@@ -1,7 +1,7 @@
 import { ref, type Ref } from 'vue'
 
 type Language = Ref<'en' | 'es'>
-const language: Language = ref('en')
+const language: Language = ref('es')
 const languageKey = Symbol('language')
 function swapLanguage() {
   language.value = language.value === 'en' ? 'es' : 'en'
